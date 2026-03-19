d670c552791476211e44bef3c236e5c858c92a0e
Konstantin <drahmin5@icloud.com>
2026-03-19 21:08:36 +0300

    after profile

--- sem_2/arc/hw_6/main.py
+++ sem_2/arc/hw_6/main.py
@@ -1,32 +1,31 @@
 import requests
+import re
+from collections import Counter
 
 def get_text(url):
     response = requests.get(url)
     return response.text
 
-def count_word_frequencies(url, word):
-    text = get_text(url)
-    words = text.split()
-    count = 0
-    for w in words:
-        if w == word:
-            count += 1
-    return count
+def count_word_frequencies(text, words_to_count):
+    clean_text = re.sub(r"[^\w\s]", " ", text.lower())
+    
+    counted_words = Counter(clean_text.split())
+
+    return counted_words
+
+def words_from_file(name):
+    with open(name, 'r', encoding="utf-8") as file:
+        return [line.strip() for line in file if line.strip()]
+    
 
 def main():
     words_file = "words.txt"
     url = "https://eng.mipt.ru/why-mipt/"
+    text = get_text(url)
 
-    words_to_count = []
-    with open(words_file, 'r') as file:
-        for line in file:
-            word = line.strip()
-            if word:
-                words_to_count.append(word)
+    words_to_count = words_from_file(words_file)
 
-    frequencies = {}
-    for word in words_to_count:
-        frequencies[word] = count_word_frequencies(url, word)
+    frequencies = count_word_frequencies(text, words_to_count)
     
     print(frequencies)
 
